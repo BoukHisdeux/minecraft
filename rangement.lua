2d0b032199784787a20efd6f5b562946382859e2
BoukHisdeux <141704524+BoukHisdeux@users.noreply.github.com>
2024-06-05 11:01:07 +0200

    Update rangement.lua

--- rangement.lua
+++ rangement.lua
@@ -1,12 +1,12 @@
 -- Programme pour turtle de mise en bloc des blocs de Cobblestone
 
---int r()
-r(1)=4
-r(2)=8
-r(3)=12
-r(4)=13
-r(5)=14
-r(6)=15
-r(7)=16
+local int r{}
+r[1]=4
+r[2]=8
+r[3]=12
+r[4]=13
+r[5]=14
+r[6]=15
+r[7]=16
 
-turtle.select(r(1))
+turtle.select(r[1])
