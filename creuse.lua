41bf2138a39551e5824473bc64a6b0eadbe68797
BoukHisdeux <141704524+BoukHisdeux@users.noreply.github.com>
2024-06-06 16:48:28 +0200

    Version 1.0

--- creuse.lua
+++ creuse.lua
@@ -1,5 +1,7 @@
 -- version 0.2
 decallage = 4 -- 12 
+deplacement = 2 -- lar-- version 1.0
+decallage = 6 -- 12 
 deplacement = 2 -- largeur de minage
 longueur = 8 -- 64   -- longueur de minage 
 rPremier = 0
@@ -35,37 +37,41 @@ function Minage()
       digDownModerne()
     for j=1,longueur do
       digUpModerne()
-        digDownModerne()
-        digModerne()
+      digDownModerne()
+      digModerne()
       turtle.forward()
     end
+
+    digUpModerne()
+    digDownModerne()
+
     for j=1,longueur do
       turtle.back()
     end
-    turtle.turnLeft()
+
+    if i ~= deplacement then
+      turtle.turnLeft()
       digModerne()
-        turtle.forward()
-        turtle.turnRight()
-  end
-  turtle.forward()
-  for i=1,deplacement do
-    turtle.forward()
+      turtle.forward()
+      turtle.turnRight()
+    end
   end
   turtle.turnLeft()
 end
 
-for descente=rPremier, rDernier do
+for descente=rDernier,rPremier, -1 do
   turtle.select(1)
   for i=1,decallage do
+    digModerne()
     turtle.forward()
   end
 
-  for i=0,rDernier-descente do
+  for i=0,descente do
     digDownModerne()
       turtle.down()
-      digDownModerne()
+    digDownModerne()
       turtle.down()
-      digDownModerne()
+    digDownModerne()
       turtle.down()
   end
   turtle.up()
@@ -74,13 +80,25 @@ for descente=rPremier, rDernier do
 
   Minage()
 
-  turtle.turnLeft()
+  for i=1,deplacement-1 do
+    turtle.back()
+  end
+
+  turtle.down()
+
+  for i=0,descente do
+    turtle.up()
+    turtle.up()
+    turtle.up()
+  end
+--  turtle.down()
   
   for i=1,decallage do
     turtle.back()
   end
 
   while not turtle.detectUp() do
+    digUpModerne()
     turtle.up()
   end
 
